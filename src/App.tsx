cd31763f2314859ce19419a86f9a8b10239b627c
Ayush0054 <ayush5aprill@gmail.com>
2023-11-02 03:03:42 +0530

    ably text

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import Spaces from "@ably/spaces";
 import { SpaceProvider, SpacesProvider } from "@ably/spaces/react";
 import LiveCursors from "./components/LiveCursors";
 
-
 const App = ({ spaces }: { spaces: Spaces }) => (
   <SpacesProvider client={spaces}>
     <SpaceProvider name="live-cursors">
+      <h1 className=" text-red-600 text-2xl bg-black text-right pt-2 pr-2  ">
+        Powered by Ably realtime
+      </h1>
       <LiveCursors />
     </SpaceProvider>
   </SpacesProvider>
