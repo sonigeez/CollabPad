877af17fb62a2453adaed4a1e7a98cf97b71d4a0
Bharat Soni <bharatchills@gmail.com>
2023-11-02 02:14:20 +0530

    fix:rollup deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {},
+  build: {
+    rollupOptions: {
+      external: ["styled-components"],
+    },
+  },
   plugins: [react()],
 });
